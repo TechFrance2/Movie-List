cb95015abd2db4f310ec451cd9b45836e3dbd0c0
Franci Nakamura <franci.nakamura100@gmail.com>
2020-05-29 13:03:57 -0700

    Eliminated unneeded files

---
+++ src/MovieList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function MovieList() {
+    return(
+        <div>
+        <p>
+          <input type='checkbox'/>  Mulan 
+        </p>
+        <p>
+           <input type='checkbox'/>  The Pilgrim's Progress
+        </p> 
+        <p>
+           <input type='checkbox'/> Wonder Woman 1984  
+        </p>
+        <p>
+           <input type='checkbox'/> The Invisible Man  
+        </p>
+        <p>
+           <input type='checkbox'/> No Time to Die  
+        </p>
+
+      </div>
+
+
+    )
+}
+
+
+export default MovieList
