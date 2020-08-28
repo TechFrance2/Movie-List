41d7d36578fd9cbfe1145473a97e240c67e4a3d7
Franci Nakamura <franci.nakamura100@gmail.com>
2020-08-27 19:48:37 -0700

    created event handler

---
+++ src/moviedata.js
@@ -0,0 +1,38 @@
+const moviedata = [
+    {
+        id: 1,
+        text: "Mulan",
+        saw: false
+    },
+    {
+        id: 2, 
+        text: "The Pilgrim's Progress",
+        saw: false
+    },
+    {
+        id: 3, 
+        text: "Wonder Woman 1984",
+        saw: false
+    },
+    {
+        id: 4,
+        text: "The Invisible Man",
+        saw: true
+    },
+    {
+        id: 5, 
+        text: "No Time to Die",
+        saw: false
+    },
+    {   id: 6, 
+        text: "Parasite",
+        saw: true  
+    },
+    {
+        id: 7, 
+        text: "Casablanca",
+        saw: true
+    }  
+]
+
+export default moviedata
